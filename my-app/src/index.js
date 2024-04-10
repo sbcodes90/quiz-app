f8dcd89275184adb77741bde383f89fdf1337c5c
Burrell, Sydney (Contractor) <Sydney_Burrell@comcast.com>
2024-04-10 11:54:09 -0400

    install react router dom

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,11 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Test from './components/Test';
+
+
+const router = createBrowserRouter([{
+  path: '/',
+  element: <App />
+},
+{
+  path: 'test',
+  element: <Test />
+}]);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 

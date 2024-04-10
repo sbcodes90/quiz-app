c0a176e5ebeb7976d2c7bddd3d7b674f266219ff
Burrell, Sydney (Contractor) <Sydney_Burrell@comcast.com>
2024-04-10 10:17:55 -0400

    install create-react-app and tailwind css

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="text-center text-purple-700 font-black text-2xl">Bebe's World Logo</div>
+    </div>
+  );
+}
+
+export default App;

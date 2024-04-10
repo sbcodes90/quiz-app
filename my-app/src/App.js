faeaa5a10f12d2b2e8a52a67cbb9c73773742fbb
Burrell, Sydney (Contractor) <Sydney_Burrell@comcast.com>
2024-04-10 10:58:34 -0400

    install react router

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,7 @@
 
 function App() {
   return (
-    <div className="App">
+    <div className="min-h-[100vh] bg-teal-500">
       <div className="text-center text-purple-700 font-black text-2xl">Bebe's World Logo</div>
     </div>
   );

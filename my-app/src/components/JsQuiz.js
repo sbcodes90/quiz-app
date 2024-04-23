b8765a9e40eeb05e050e7d80923169e87e11411e
sbcodes90 <sydneycburrell29@gmail.com>
2024-04-23 19:58:21 -0400

    css updates

--- my-app/src/components/JsQuiz.js
+++ my-app/src/components/JsQuiz.js
@@ -6,15 +6,6 @@ export default function JsQuiz() {
       <header className="text-2xl text-white font-black text-center pt-10">
         Javascript
       </header>
-{/* 
-      <div class="grid grid-rows-3 grid-flow-col gap-4">
-        <div class="row-span-3 ...">
-          <img src={dog} alt="dog" className="w-[500px]"/>
-        </div>
-        <div class="col-span-2 pt-10 bg-white mr-20 mt-10 p-5 rounded-3xl">Question</div>
-        <div class="row-span-2 col-span-2 bg-yellow-500 h-fit mr-20 rounded-3xl p-5">Answer Option One</div>
-        
-      </div> */}
       <div className="grid grid-cols-2">
         <div><img src={dog} alt="dog" /></div>
         <div className="grid-cols-2 mr-20 mt-[150px]">

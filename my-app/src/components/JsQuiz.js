a8bdb44a9fdf774fdb96418d214ae283006868d3
sbcodes90 <sydneycburrell29@gmail.com>
2024-04-23 19:58:03 -0400

    css updates

--- my-app/src/components/JsQuiz.js
+++ my-app/src/components/JsQuiz.js
@@ -1,26 +1,28 @@
 import React from "react";
-import bg from "../images/quiz-background.png";
 import dog from "../images/dog.png";
-import treat from "../images/dogtreat.png";
-import bebe from "../images/bebe.png";
 export default function JsQuiz() {
   return (
     <div className=" bg-gradient-to-r from-purple-950 to-purple-800 min-h-screen">
-      <div className="text-white text-3xl font-black text-center pt-10">
+      <header className="text-2xl text-white font-black text-center pt-10">
         Javascript
-      </div>
-      {/* <div className='flex flex-col-1 lg:col-2 items-center justify-around gap-5 pt-20'> */}
-      <div className="grid grid-cols-1 lg:grid-cols-2 place-content-center px-10 py-10">
-        <div className="lg:w-[500px]">
-        <img src={treat} alt="dog-treat" />
+      </header>
+{/* 
+      <div class="grid grid-rows-3 grid-flow-col gap-4">
+        <div class="row-span-3 ...">
+          <img src={dog} alt="dog" className="w-[500px]"/>
         </div>
-        <div className="bg-white lg:w-[500px] lg:h-[400px] rounded-2xl p-10 shadow-2xl">
-        <div className="font-bold text-black text-center">Uh oh! Bebe is hungry!</div>
-        <div className="text-center text-purple-800 font-bold mt-2">Which code will feed her a snack?</div>
-        </div>
-     {/*    <button className="mt-10 py-5 rounded-3xl mb-20 bg-yellow-400 font-extrabold">
-          Lets Play
-        </button> */}
+        <div class="col-span-2 pt-10 bg-white mr-20 mt-10 p-5 rounded-3xl">Question</div>
+        <div class="row-span-2 col-span-2 bg-yellow-500 h-fit mr-20 rounded-3xl p-5">Answer Option One</div>
+        
+      </div> */}
+      <div className="grid grid-cols-2">
+        <div><img src={dog} alt="dog" /></div>
+        <div className="grid-cols-2 mr-20 mt-[150px]">
+         <div className="bg-white rounded-3xl p-5"> Question </div>
+         <div className="bg-yellow-500 text-center rounded-3xl mt-2 p-5"> Answer </div>
+         <div className="bg-yellow-500 text-center rounded-3xl mt-2 p-5"> Answer </div>
+          </div>
+
       </div>
     </div>
   );

4658f4e082edfb9ffdc58defdeb2ef4560478a8a
sbcodes90 <sydneycburrell29@gmail.com>
2024-04-23 20:36:19 -0400

    responsive fix

--- my-app/src/components/JsQuiz.js
+++ my-app/src/components/JsQuiz.js
@@ -3,7 +3,7 @@ import dog from "../images/dog.png";
 import { questions } from "../utils/data.js";
 export default function JsQuiz() {
 
-  const [ourQuestions, setOurQuestions] = useState(0);
+  const [currQuestion, setCurrQuestion] = useState(0);
 
 
   return (
@@ -11,12 +11,12 @@ export default function JsQuiz() {
       <header className="text-2xl text-white font-black text-center pt-10">
         Javascript
       </header>
-      <div className="grid grid-cols-2">
+      <div className="grid lg:grid-cols-2">
         <div>
           <img src={dog} alt="dog" />
         </div>
-        <div className="grid-cols-2 mr-20 mt-[150px]">
-          <div className="bg-white rounded-3xl p-5 font-bold"> {questions[0]?.questionText} </div>
+        <div className="grid-cols-2 mx-20 mb-10 lg:mr-20 lg:mt-[150px]">
+          <div className="bg-white rounded-3xl p-5 font-bold"> {questions[currQuestion]?.questionText} </div>
          {questions[0]?.answerOptions.map((answerOption) => (
             <button key={answerOption.answerText} className="bg-yellow-500 text-center rounded-2xl mt-2 p-5 font-bold w-full hover:bg-teal-600"> 
            {answerOption.answerText}

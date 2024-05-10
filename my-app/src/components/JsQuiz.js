0eaba906a88e8d0d7894de85bc866df3d495f240
sbcodes90 <sydneycburrell29@gmail.com>
2024-05-09 22:24:12 -0400

    css fix

--- my-app/src/components/JsQuiz.js
+++ my-app/src/components/JsQuiz.js
@@ -32,7 +32,7 @@ export default function JsQuiz() {
   return (
     <>
       <div className=" bg-gradient-to-r from-purple-950 to-purple-800 min-h-screen">
-        <header className="text-2xl text-black font-black text-center p-5 bg-purple-500">
+        <header className="fixed top-0 w-full text-2xl text-black font-black text-center p-5 bg-purple-500">
           JS Quiz
         </header>
         <div className="grid lg:grid-cols-2">
@@ -41,7 +41,7 @@ export default function JsQuiz() {
           </div>
 
           {!showScore ? (
-            <div className="grid-cols-2 mx-20 mb-10 lg:mr-20 lg:mt-[150px]">
+            <div className="grid-cols-2 mx-20 mb-20 lg:mr-20  lg:mt-[150px]">
               <div className="bg-white rounded-3xl p-5 font-black">
                 {questions[currentQuestion]?.questionText}
               </div>
@@ -58,10 +58,10 @@ export default function JsQuiz() {
               ))}
             </div>
           ) : (
-            <div className="text-white font-black text-2xl text-center lg:mt-[120px] lg:mr-10 lg:mb-40 rounded-3xl pt-[100px]">
+            <div className="text-white font-black text-2xl text-center lg:mt-[120px] lg:mr-10 lg:mb-40 rounded-3xl pt-0 lg:pt-[100px] mb-20 p-10">
               {score === 4 && (
                 <>
-                  <div className="mb-10">Congrats Friend! You did it! You scored 100%!</div>
+                  <div className="mb-10 lg:mb-10">Congrats Friend! You scored 100%!</div>
                   <Link
                     to="/options"
                     className="px-10 py-5 bg-teal-400 rounded-3xl text-sm text-black hover:bg-yellow-500"
